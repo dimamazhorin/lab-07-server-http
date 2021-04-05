c34dad946319792447858da410e7b8861d9dfe8a
dimamazhorin <dmirtymazhorinjr@gmail.com>
2021-04-06 00:14:24 +0300

    F

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,10 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2021 ak-kuznetsov <a.kuznetsov2701@mail.ru>
 
-#include <header.hpp>
+#include <Client.hpp>
+int main(int argc, char* argv[]) {
+  Client client(argc, argv);
+  client.Run();
+}
+
+//./cmake-build-debug/demo  http://localhost:8080/v1/api/suggest
+//{\"input\":\"hel\"}
